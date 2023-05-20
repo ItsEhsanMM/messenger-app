6438aa90ccd76b05795abf73abe2d9da6ade99b8
ItsEhsanMM <itsehsanmm@gmail.com>
2023-05-20 21:08:34 +0330

    error connection in login page added

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import firebase from "firebase/compat/app";
 import { auth } from "../services/firebase";
 
@@ -11,6 +11,8 @@ import styles from "./Login.module.scss";
 document.title = "Login Page";
 
 const Login = () => {
+   const [error, setError] = useState("");
+
    return (
       <div className={styles.container}>
          <div className={styles.aContainer}>
@@ -22,13 +24,18 @@ const Login = () => {
                   <h3>Login with :</h3>
                </div>
                <div
-                  onClick={() => auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())}
+                  onClick={() =>
+                     auth
+                        .signInWithRedirect(new firebase.auth.GoogleAuthProvider())
+                        .catch(() => setError("Error network connection"))
+                  }
                   className={styles.logoContainer}
                >
                   <img src={google} alt="google" />
                </div>
             </div>
          </div>
+         {error && <h3 style={{ fontSize: "2rem", color: "red" , marginTop: '30px' }}>{error}</h3>}
       </div>
    );
 };
